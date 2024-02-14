31c85c747081e81726df7bacb26f7c9a748dc4f1
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 12:54:42 +0530

    Added StoryProgress and Story Viewer

--- src/components/Story/StoryProgress.jsx
+++ src/components/Story/StoryProgress.jsx
@@ -1,40 +1,40 @@
-// import React, { useState, useEffect } from "react";
-// import "./StoryProgress.css";
+import React, { useState, useEffect } from "react";
+import "./StoryProgress.css";
 
-// const StoryProgressBar = ({ duration, index, activeIndex, setActiveIndex }) => {
-//   const [progress, setProgress] = useState(0);
+const StoryProgressBar = ({ duration, index, activeIndex, setActiveIndex }) => {
+  const [progress, setProgress] = useState(0);
 
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setProgress((prevProgress) => {
-//         if (prevProgress < 100) {
-//           return prevProgress + 1;
-//         }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress < 100) {
+          return prevProgress + 1;
+        }
 
-//         clearInterval(interval);
-//         return prevProgress;
-//       });
-//     }, duration / 100);
+        clearInterval(interval);
+        return prevProgress;
+      });
+    }, duration / 100);
 
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, [duration, activeIndex]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [duration, activeIndex]);
 
-//   useEffect(() => {
-//     setProgress(0);
-//   }, [activeIndex]);
+  useEffect(() => {
+    setProgress(0);
+  }, [activeIndex]);
 
-//   const isActive = index === activeIndex;
+  const isActive = index === activeIndex;
 
-//   return (
-//     <div className={`progress-bar-container ${isActive ? "active" : ""}`}>
-//       <div
-//         className={`${isActive ? "progress-bar" : ""}`}
-//         style={{ width: ` ${progress}%` }}
-//       ></div>
-//     </div>
-//   );
-// };
+  return (
+    <div className={`progress-bar-container ${isActive ? "active" : ""}`}>
+      <div
+        className={`${isActive ? "progress-bar" : ""}`}
+        style={{ width: ` ${progress}%` }}
+      ></div>
+    </div>
+  );
+};
 
-// export default StoryProgressBar;
+export default StoryProgressBar;
