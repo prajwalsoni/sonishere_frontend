252359ac06cbecb56aff56fd98707325745c0530
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 12:07:02 +0530

    Initial Commit

---
+++ src/components/Story/StoryData.js
@@ -0,0 +1,32 @@
+// export const story = [
+//     {
+//       image:
+//         "https://cdn.pixabay.com/photo/2020/09/04/20/09/cartoon-5544856_640.jpg",
+//       username: "Ankit",
+//       userId: 1,
+//     },
+//     {
+//       image:
+//         "https://cdn.pixabay.com/photo/2019/10/17/09/18/cartoon-4556429_640.png",
+//       username: "Sarvesh",
+//       userId: 1,
+//     },
+//     {
+//       image:
+//         "https://cdn.pixabay.com/photo/2022/11/23/18/31/birds-7612651_640.png",
+//       username: "Sambit",
+//       userId: 1,
+//     },
+//     {
+//       image:
+//         "https://cdn.pixabay.com/photo/2021/05/14/12/26/man-6253257_640.jpg",
+//       username: "Tushar",
+//       userId: 4,
+//     },
+//     {
+//       image:
+//         "https://cdn.pixabay.com/photo/2022/05/06/17/15/cartoon-giraffe-7178753_640.jpg",
+//       username: "Shivansh",
+//       userId: 5,
+//     },
+//   ];
---
+++ src/theam/DarkTheam.jsx
@@ -0,0 +1,27 @@
+// import { createTheme } from "@mui/material";
+
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: "dark", 
+//     primary: {
+//       main: "rgb(88,199,250)",
+     
+//     },
+//     secondary: {
+//       main: "#5A20CB",
+//     },
+//     black: {
+//       main: "#242B2E",
+//     },
+//     background: {
+//       main: "#212534",
+//       default: "#212534",
+//       paper: "#212534",
+//     },
+//     textColor: {
+//       main: "#111111",
+//     },
+//   },
+// });
+
+// export default darkTheme;
