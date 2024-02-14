27090a90f9686a0a547c50724cc6c1ab80afd081
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 13:06:08 +0530

    added reels

--- src/Redux/Reels/reels.acton.js
+++ src/Redux/Reels/reels.acton.js
@@ -1,76 +1,76 @@
-// import { api } from "../../config/api"
-// import { CREATE_REELS_FAILUER, CREATE_REELS_REQUEST, CREATE_REELS_SUCCESS, GET_ALL_REELS_FAILUER, GET_ALL_REELS_REQUEST, GET_ALL_REELS_SUCCESS, GET_USERS_REEL_FAILUER, GET_USERS_REEL_REQUEST, GET_USERS_REEL_SUCCESS } from "./reels.actionType"
+import { api } from "../../config/api"
+import { CREATE_REELS_FAILUER, CREATE_REELS_REQUEST, CREATE_REELS_SUCCESS, GET_ALL_REELS_FAILUER, GET_ALL_REELS_REQUEST, GET_ALL_REELS_SUCCESS, GET_USERS_REEL_FAILUER, GET_USERS_REEL_REQUEST, GET_USERS_REEL_SUCCESS } from "./reels.actionType"
 
-// export const createReels=(reelData)=>async (dispatch)=>{
-//     dispatch({type:CREATE_REELS_REQUEST})
-//     try {
+export const createReels=(reelData)=>async (dispatch)=>{
+    dispatch({type:CREATE_REELS_REQUEST})
+    try {
 
-//         const {data} = await api.post("/api/reels",reelData)
+        const {data} = await api.post("/api/reels",reelData)
 
-//         dispatch({type:CREATE_REELS_SUCCESS,payload:data})
+        dispatch({type:CREATE_REELS_SUCCESS,payload:data})
 
-//         console.log("created reels",data)
+        console.log("created reels",data)
 
         
-//     } catch (error) {
-//         console.log("catch error ",error)
-//         dispatch({type:CREATE_REELS_FAILUER,payload:error})
-//     }
-// }
+    } catch (error) {
+        console.log("catch error ",error)
+        dispatch({type:CREATE_REELS_FAILUER,payload:error})
+    }
+}
 
-// export const getAllReels=()=>async (dispatch)=>{
-//     dispatch({type:GET_ALL_REELS_REQUEST})
-//     try {
+export const getAllReels=()=>async (dispatch)=>{
+    dispatch({type:GET_ALL_REELS_REQUEST})
+    try {
 
-//         const {data} = await api.get("/api/reels")
+        const {data} = await api.get("/api/reels")
 
-//         dispatch({type:GET_ALL_REELS_SUCCESS,payload:data})
+        dispatch({type:GET_ALL_REELS_SUCCESS,payload:data})
 
-//     } catch (error) {
-//         console.log("catch error ",error)
-//         dispatch({type:GET_ALL_REELS_FAILUER,payload:error})
-//     }
-// }
+    } catch (error) {
+        console.log("catch error ",error)
+        dispatch({type:GET_ALL_REELS_FAILUER,payload:error})
+    }
+}
 
-// export const getUsersReels=(userId)=>async (dispatch)=>{
-//     dispatch({type:GET_USERS_REEL_REQUEST})
-//     try {
+export const getUsersReels=(userId)=>async (dispatch)=>{
+    dispatch({type:GET_USERS_REEL_REQUEST})
+    try {
 
-//         const {data} = await api.get(`/api/reels/user/${userId}`)
+        const {data} = await api.get(`/api/reels/user/${userId}`)
 
-//         dispatch({type:GET_USERS_REEL_SUCCESS,payload:data})
+        dispatch({type:GET_USERS_REEL_SUCCESS,payload:data})
 
-//     } catch (error) {
-//         console.log("catch error ",error)
-//         dispatch({type:GET_USERS_REEL_FAILUER,payload:error})
-//     }
-// }
+    } catch (error) {
+        console.log("catch error ",error)
+        dispatch({type:GET_USERS_REEL_FAILUER,payload:error})
+    }
+}
 
-// // export const likePost=(postId)=>async (dispatch)=>{
-// //     dispatch({type:LIKE_POST_REQUEST})
-// //     try {
+export const likePost=(postId)=>async (dispatch)=>{
+    dispatch({type:LIKE_POST_REQUEST})
+    try {
 
-// //         const {data} = await api.put(`/api/posts/like/${postId}`)
+        const {data} = await api.put(`/api/posts/like/${postId}`)
 
-// //         dispatch({type:LIKE_POST_SUCCESS,payload:data})
+        dispatch({type:LIKE_POST_SUCCESS,payload:data})
 
-// //     } catch (error) {
+    } catch (error) {
 
-// //         console.log("catch error ",error)
-// //         dispatch({type:LIKE_POST_FAILUER,payload:error})
-// //     }
-// // }
+        console.log("catch error ",error)
+        dispatch({type:LIKE_POST_FAILUER,payload:error})
+    }
+}
 
-// // export const savePost=(postId)=>async (dispatch)=>{
-// //     dispatch({type:SAVE_POST_REQUEST})
-// //     try {
+export const savePost=(postId)=>async (dispatch)=>{
+    dispatch({type:SAVE_POST_REQUEST})
+    try {
 
-// //         const {data} = await api.put(`/api/posts/${postId}/save`)
+        const {data} = await api.put(`/api/posts/${postId}/save`)
 
-// //         dispatch({type:SAVE_POST_SUCCESS,payload:data})
+        dispatch({type:SAVE_POST_SUCCESS,payload:data})
         
-// //     } catch (error) {
-// //         console.log("catch error ",error)
-// //         dispatch({type:SAVE_POST_FAILUER,payload:error})
-// //     }
-// // }
+    } catch (error) {
+        console.log("catch error ",error)
+        dispatch({type:SAVE_POST_FAILUER,payload:error})
+    }
+}
