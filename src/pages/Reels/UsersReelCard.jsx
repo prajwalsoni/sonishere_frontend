dcedea0e37d159201bbf25439952724a031010da
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 13:29:34 +0530

    Added Reels File

--- src/pages/Reels/UsersReelCard.jsx
+++ src/pages/Reels/UsersReelCard.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
+import React from "react";
 
-// const UsersReelCard = ({ reel }) => {
-//   return (
-//     <div className="w-[15rem] px-2">
-//       <video
-//         className="w-full h-full"
-//         src={reel.video}
-//         controls
-//       />
-//     </div>
-//   );
-// };
+const UsersReelCard = ({ reel }) => {
+  return (
+    <div className="w-[15rem] px-2">
+      <video
+        className="w-full h-full"
+        src={reel.video}
+        controls
+      />
+    </div>
+  );
+};
 
-// export default UsersReelCard;
+export default UsersReelCard;
